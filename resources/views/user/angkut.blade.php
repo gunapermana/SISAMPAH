218d740ad7cc9a7342cb71c9575d7910b38f5fcb
gunapermana <gunapermana13@gmail.com>
2022-10-05 19:47:15 +0800

    test

--- resources/views/user/angkut.blade.php
+++ resources/views/user/angkut.blade.php
@@ -6,20 +6,21 @@
   <div class="row align-items-center">
     <div class="col-sm-6">
       <h1 class="fs-1 mb-5 fw-bold">ANGKUT SAMPAH</h1>
-      <form class="form-angkut p-2 rounded-4">
+      <form class="form-angkut p-4 rounded-4">
         <div class="row">
           <label for="inputPassword" class="col-sm-3 col-form-label">Kepala Keluarga</label>
           <div class="col-sm-9 mb-3">
           <input type="KK" class="form-control" id="exampleFormControlInput1">
           </div>
-          
-          <label for="inputPassword" class="col-sm-3 col-form-label">Password</label>
-          <div class="col-sm-9 mb-3">
-            <input type="password" class="form-control" id="inputPassword">
+
+          <label for="inputPassword" class="col-sm-3 col-form-label">NIK</label>
+          <div class="col-sm-9">
+          <input type="NIK" class="form-control" id="exampleFormControlInput1">
           </div>
+
         </div>
       </form>
-      <button type="submit" class="btn btn-primary">Submit</button>
+      <button type="submit" class="btn btn-primary mt-3">Submit</button>
     </div>
     <div class="col-sm-6">
       <img src="./images/angkut.png" class="img-fluid" alt="Home Image">
